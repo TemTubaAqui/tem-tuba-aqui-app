a003c4360d639616ed4fc61326e90999f1735f8b
Thiago Brito <brito.lol10@gmail.com>
2023-08-20 17:22:20 -0300

    botao de emergencia feito

---
+++ src/components/EmergencyButton.tsx
@@ -0,0 +1,30 @@
+
+import { TouchableOpacity, Image, Linking } from 'react-native';
+
+export function EmergencyButton() {
+    const handlePress = () => {
+        Linking.openURL('tel:193');
+    };
+
+    return (
+        <TouchableOpacity onPress={handlePress}
+            style={{
+                width: 65,
+                height: 65,
+                borderRadius: 50,
+                position: 'absolute',
+                bottom: 95,
+                right: 13,
+                backgroundColor: 'red',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}
+        >
+            <Image
+                source={require('../assets/Sos.png')}
+                alt='botão que possue uma imagem de um telefone e SOS'
+
+            />
+        </TouchableOpacity>
+    );
+}
