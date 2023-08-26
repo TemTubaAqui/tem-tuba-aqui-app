5862b5003c303bb484900cc3f6c4d28232032c06
ulisses.silva <ulisses.silva@mv.com.br>
2023-08-26 17:47:07 -0300

    ajuste no layout e estilo

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { NativeBaseProvider, Box } from "native-base";
+import { NativeBaseProvider, extendTheme } from "native-base";
 import { EmergencyButton } from './src/components/EmergencyButton';
 import Header from "./src/components/Header";
 import BarStatus from "./src/components/BarStatus";
 import { AppRoutes } from "./src/routes/app.routes";
 
+const theme = extendTheme({
+  colors: {
+    primary: "#3589FF", 
+    background: "#FFFEED", // "#FFFFFF"
+    card: "#FFFFFF"
+  },
+});
+
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
       <BarStatus />
       <Header />
       <AppRoutes />
