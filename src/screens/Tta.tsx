6a1b8dc717c1490475b529422c8ce3f0f3e8e31d
joao.assis <joao.ctassis@gmail.com>
2023-08-31 19:47:11 -0300

    Revert "tela info"
    This reverts commit 731d77e08e7c66d49f3df74ddf3e4e36579f4abe.

--- src/screens/Tta.tsx
+++ src/screens/Tta.tsx
@@ -1,24 +1,9 @@
-import { Text, View, Image, TextInput, ScrollView } from 'react-native';
-
-import { Beach } from '../components/Beach';
+import { Text, View } from 'react-native';
 
 export default function Tta() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', marginTop: 9 }}>
-      <Text>Tem tuba aqui?</Text>
-      <View style={{
-        flexDirection: 'row', gap: 12, paddingHorizontal: 23, paddingVertical: 8, borderRadius: 20,
-        backgroundColor: "#EDEDED", marginTop: 33, marginBottom: 18
-      }}>
-        <Image source={require('../assets/Search.png')}
-          alt='botão que possue uma imagem de um telefone e SOS' style={{ width: 24, height: 24 }} />
-        <TextInput placeholder='Qual praia que você deseja informação?' placeholderTextColor={'#000'} />
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Tta!</Text>
       </View>
-
-      <ScrollView >
-        <Beach name={'Porto de galinhas'} />
-
-      </ScrollView>
-    </View>
-  );
-}
+    );
+  }
