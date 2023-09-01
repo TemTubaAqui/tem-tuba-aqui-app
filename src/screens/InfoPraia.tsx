5e835816eff7e652726ed35ae6353b28aded1177
André Felipe <dede.f99.80@gmail.com>
2023-08-31 21:54:31 -0300

    tela infoPraia finalizada

--- src/screens/InfoPraia.tsx
+++ src/screens/InfoPraia.tsx
@@ -1,12 +1,11 @@
-import { TouchableOpacity, Image } from "react-native";
+import { TouchableOpacity } from "react-native";
 import React, { useState, useEffect } from "react";
 import BeachService from "../services/beach";
-import { Box, HStack, ScrollView, VStack, Text } from "native-base";
+import { Box, HStack, ScrollView, VStack, Text, Image } from "native-base";
 import { AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/core";
 import praia from "../assets/praia.jpeg";
 import { format } from "date-fns";
-
 
 const mockData = {
   id: 33,
@@ -20,17 +19,9 @@ const mockData = {
   longitude: 0.0,
 };
 
-// checar se é null
-if (mockData.last_attack) {
-  // formatação da data
-  mockData.last_attack = format(new Date(mockData.last_attack), "dd/MM/yyyy");
-} else {
-  mockData.last_attack = "N/A"; // valor padrão
-}
-
 export default function InfoPraia() {
   const { navigate } = useNavigation<any>();
-  const [beachData, setBeachData] = useState(null);
+  //const [beachData, setBeachData] = useState(null);
 
   // useEffect(() => {
   //   BeachService.getBeachById(beachId)
@@ -44,38 +35,37 @@ export default function InfoPraia() {
 
   return (
     <Box w="100%" flex={1} bg={"background"}>
-        <HStack
-          w="100%"
-          alignItems="flex-start"
-          justifyContent="flex-start"
-          px={4}
-          marginTop={5}
-        >
-          <TouchableOpacity onPress={() => navigate("Tta")}>
-            <AntDesign name="leftcircle" size={30} color="#00689D" />
-          </TouchableOpacity>
-        </HStack>
+      <HStack
+        w="100%"
+        alignItems="flex-start"
+        justifyContent="flex-start"
+        px={4}
+        marginTop={5}
+      >
+        <TouchableOpacity onPress={() => navigate("Tta")}>
+          <AntDesign name="leftcircle" size={30} color="#00689D" />
+        </TouchableOpacity>
+      </HStack>
+      <VStack
+        backgroundColor={"card"}
+        borderRadius={15}
+        shadow={"7"}
+        mx={5}
+        my={5}
+        marginTop={5}
+        flex={1}
+      >
+        <Image
+          source={praia}
+          alt="Praia"
+          w={"100%"}
+          height={"40%"}
+          borderTopLeftRadius={15}
+          borderTopRightRadius={15}
+        />
 
-        <VStack
-          backgroundColor={"card"}
-          borderRadius={15}
-          shadow={"7"}
-          //p={4}
-          mx={5}
-          my={5}
-          marginTop={5}
-        >
-          <Image
-            source={praia}
-            alt="Curiosidades"
-            style={{
-              width: "100%",
-              height: "40%",
-              borderTopLeftRadius: 15,
-              borderTopRightRadius: 15,
-            }}
-          />
-          <Box width="100%" alignItems={"center"}>
+        <ScrollView>
+          <VStack width="100%" alignItems={"center"}>
             <Text
               marginTop={8}
               fontSize={22}
@@ -84,8 +74,8 @@ export default function InfoPraia() {
             >
               Informações da Praia
             </Text>
-          </Box>
-          <Box mx={5}>
+          </VStack>
+          <VStack mx={5}>
             <Text
               fontWeight="bold"
               marginTop={5}
@@ -104,7 +94,7 @@ export default function InfoPraia() {
               Cidade/Estado:
             </Text>
             <Text fontSize={16}>
-              {mockData.city}/ {mockData.state}{" "}
+              {mockData.city}/ {mockData.state}
             </Text>
             <Text
               fontWeight="bold"
@@ -123,9 +113,14 @@ export default function InfoPraia() {
             >
               Último Registro de Ataque:
             </Text>
-            <Text fontSize={16}>{mockData.last_attack} </Text>
-          </Box>
-        </VStack>
+            <Text fontSize={16}>
+              {mockData.last_attack !== "null"
+                ? format(new Date(mockData.last_attack), "dd/MM/yyyy")
+                : "N/A"}
+            </Text>
+          </VStack>
+        </ScrollView>
+      </VStack>
     </Box>
   );
 }
