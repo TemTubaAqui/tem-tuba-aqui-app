b9dae0ae37852ec244df8ab7eddf02ffdf66381b
Thiago Brito <brito.lol10@gmail.com>
2023-09-04 20:39:46 -0300

    Juntando com API

--- src/components/Beach.tsx
+++ src/components/Beach.tsx
@@ -5,31 +5,32 @@ import { useNavigation } from '@react-navigation/core';
 
 
 type Props = TouchableOpacityProps & {
-    name: String;
+  name: String;
 };
 
 export function Beach({ name, ...rest }: Props) {
-    const { navigate } = useNavigation<any>();
+  const { navigate } = useNavigation<any>();
 
-    return (
-        <Pressable onPress={() => navigate("Curiosidades")}> 
-        <VStack
-          borderColor="gray.200"
-          borderWidth={3}
-          borderRadius={8}
-          p={4}
-          mx={5}
-          justifyContent="center"
-          alignItems="center"
-        >
-         
-          <Image  source={require('../assets/Beachicon.png')} alt="Dúvida" m="auto"/>
-          <Text fontWeight="bold" fontSize={16}>
+  return (
+    <Pressable onPress={() => navigate("Curiosidades")}>
+      <VStack
+        borderColor="gray.200"
+        borderWidth={3}
+        borderRadius={8}
+        p={4}
+        mx={5}
+        justifyContent="center"
+        alignItems="center"
+        marginBottom={15}
+      >
+
+        <Image source={require('../assets/Beachicon.png')} alt="Dúvida" m="auto" />
+        <Text fontWeight="bold" fontSize={16}>
           {name}
-          </Text>
-        </VStack>
-      </Pressable>
+        </Text>
+      </VStack>
+    </Pressable>
 
-    );
+  );
 
 }
