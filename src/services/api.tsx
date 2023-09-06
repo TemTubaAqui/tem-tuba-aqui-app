df4dfdd9afb4316853e1c9978684265e602f6cf3
joao.assis <joao.ctassis@gmail.com>
2023-09-05 22:03:41 -0300

    API funcional

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const api = axios.create({
   //baseURL: "http://localhost:8000/api/v1/",
-  baseURL: "http://192.168.31.18:8000/api/v1/",
+  //baseURL: "http://192.168.31.18:8000/api/v1/",
+  baseURL: "http://ec2-3-144-41-90.us-east-2.compute.amazonaws.com:8000/api/v1/",
   headers: {
     "Content-type": "application/json",
   },
